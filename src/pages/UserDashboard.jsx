46b2d90358e4387b6341b8d9aca44cff103e516c
Atharva <atharvakhadake759@gmail.com>
2025-09-13 18:59:42 +0530

    ui fix

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -7,6 +7,7 @@ import Switch from "@mui/joy/Switch";
 import { useState, useEffect } from "react";
 import { FaLock,FaLockOpen } from "react-icons/fa6";
 
+
 const UserDashboard = () => {
   const navigate = useNavigate();
 
@@ -30,6 +31,11 @@ const UserDashboard = () => {
   const toggleLock = () => {
     const newState = !isLock;
     set(ref(database, "isLock"), newState);
+    toast.success('Door will autoclose in 5s.')
+    setTimeout(()=>{
+      set(ref(database, "isLock"), isLock);
+      toast.error('Door Closed.')
+    },5000);
   };
 
   const logout = async () => {
@@ -63,22 +69,22 @@ return (
 
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 m-6">
       
-      <div className="border-2 rounded-lg bg-blue-200 p-4 w-max h-max text-center shadow-md">
+      <div className="border-2 rounded-lg bg-blue-50 p-4 w-max h-max text-center shadow-md">
         <h3 className="font-semibold text-lg text-blue-700">Door Lock</h3>
         {isLock === null ? (
           <p className="text-gray-600 mt-2">Loading status...</p>
         ) : (
           <>
             <p className="text-gray-700 font-medium text-xl mt-2 flex justify-center items-center gap-2">
-              {isLock ? <><FaLock /> Locked</> : <><FaLockOpen/> Unlocked </>}
+              {isLock ? <><FaLock className="text-red-500" /> Locked</> : <><FaLockOpen className="text-green-500" /> Unlocked </>}
             </p>
             <Switch
-              checked={isLock}
+              checked={!isLock}
               onChange={toggleLock}
-              color="success"
               size="lg"
               variant="solid"
               className="mt-2"
+              
             />
             <p className="text-sm text-gray-500 mt-1">
               Toggle to {isLock ? "Unlock" : "Lock"}
